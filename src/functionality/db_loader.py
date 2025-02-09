5b6b1a9c47020baaac470a81be2260323d379579
Biko Nduati <bikonduati@gmail.com>
2025-02-09 12:42:17 +0300

    making preparations for chat display session manager and db loader

--- src/functionality/db_loader.py
+++ src/functionality/db_loader.py
@@ -1,12 +1,1 @@
-import requests
-
-BIN_ID = "67a65014ad19ca34f8fb9865"
-def load_json_data() -> dict:
-    """
-    Load the user json file from jsonbin.io
-    """
-
-    response = requests.get(f"https://api.jsonbin.io/v3/b/{BIN_ID}")
-    
-
-    return dict()
+# TODO - create a func to load the db from here
