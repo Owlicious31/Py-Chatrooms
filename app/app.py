38c6fff974f18cd060436899d2907a34b7a51314
Biko Nduati <bikonduati@gmail.com>
2025-03-08 21:45:53 +0300

    Scaling down: removed ui for user auth. KeyboardInterrupt is now ignored by error handler

--- app/app.py
+++ app/app.py
@@ -8,21 +8,23 @@ current_dir = os.path.dirname(__file__)
 parent_dir = os.path.join(current_dir,"..")
 sys.path.append(os.path.abspath(parent_dir))
 
-from src.ui.login_screen import LoginScreen
+from src.ui.chats_display import ChatsDisplay
 
 class App:
 
     def __init__(self) -> None:
-        self.login_screen = LoginScreen()
+        self.chat_display = ChatsDisplay()
         tkinter.Tk.report_callback_exception = self.show_error
 
     
     def run(self) -> None:
-        self.login_screen.mainloop()
+        self.chat_display.mainloop()
 
 
     def show_error(self, *args):
         err = traceback.format_exception(*args)[-1]
+        if "KeyboardInterrupt" in err:
+            return
         CTkMessagebox(title="Error", message=f"An error occured: {err}", icon="cancel")
 
 if __name__ == "__main__":
